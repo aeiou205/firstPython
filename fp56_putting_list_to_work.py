3145b0f32008d626ba20716f5e1f846fd2f8d52a
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2023-01-01 22:47:09 -0600

    Add files via upload

---
+++ fp56_putting_list_to_work.py
@@ -0,0 +1,7 @@
+vowels = ['a', 'e', 'i', 'o', 'u'] #statement vowels at vowels
+word = "Milliways" #statement word Milliways
+
+for letter in word: #take each letter in the word (9)
+    if letter in vowels:  #if some letter of the chain letter are is vowels 
+        print(letter) # so, print letter found 
+
