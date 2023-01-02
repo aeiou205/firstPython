2acd198a9bdbd2ebd446068e443166780bfdf9d0
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2023-01-01 22:47:36 -0600

    Add files via upload

---
+++ fp58_Growing_a_list_at_tuntime.py
@@ -0,0 +1,21 @@
+#Growing a list at runtime /// creciente lista en un ciclo
+
+found = [] #declaration of list found = [] 
+print(found) #impresion de la lista found
+
+#if you want add values at your list , your should do the next
+
+found.append('a') #.append open the list and add  letter 'a'
+print(len(found)) # so, print
+
+found.append('e')
+found.append('i')
+found.append('o')
+found.append('u')
+
+print(len(found))
+4
+print(found)
+
+
+
