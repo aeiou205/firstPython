87b5ec45e6765ee421ec6635ad280c2d77637067
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2023-01-01 22:54:12 -0600

    Add files via upload

---
+++ fp63-pop_obj_off_a_list.py
@@ -0,0 +1,11 @@
+nums= [1,2,4] #list with tre items
+print(nums)   #print nums
+
+nums.pop()    #poping the last element
+print(nums)   #print nums 
+
+nums.pop()   #poping the last name nums
+print(nums)  #print nums
+
+
+
