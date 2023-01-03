61d43e7e85ff50584ba3dabc9e92054b64d70604
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2023-01-02 22:18:06 -0600

    Add files via upload

---
+++ fp75-list-extend-the-square-bracket-notation.py
@@ -0,0 +1,13 @@
+saying = "don't panic!"
+
+letters = list(saying)
+print(letters)
+
+print(letters[0])
+print(letters[3])
+print(letters[6])
+print(letters[-1])
+print(letters[-3])
+print(letters[-6])
+
+
