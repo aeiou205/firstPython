f064910b2016fc22c76a352512f5fa95f4fd50b4
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2023-01-02 22:23:13 -0600

    Add files via upload

---
+++ fp90-when_not_to_use_list.py
@@ -0,0 +1,8 @@
+person1 = ['Ford Prefect','Male', 'Reseacher', 'Betelgeuse Seven']
+print(person1)
+
+
+person2 = ['Name','ford prefect','Gender', 'Male' , 'Ocupation' , 'Researcher', 'Home Pñaet', 'Beteglguese seven']
+print(person2)
+
+
