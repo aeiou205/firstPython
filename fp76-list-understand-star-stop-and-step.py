9a92299a458d20078a9c66c970656b032fbc93dd
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2023-01-02 22:18:34 -0600

    Add files via upload

---
+++ fp76-list-understand-star-stop-and-step.py
@@ -0,0 +1,16 @@
+word = "daniel"
+for beer_num in range(99,0, -1):
+    print(beer_num,word, "of beer on")
+    print(beer_num, word, "of beer")
+    print("take one down")
+
+letters = ['D','o','n',"'",'t',' ','P','A','N','I','C']
+print(letters[0:10:3])
+
+print(letters[3:]) #print position tree in 
+print(letters[:10])
+print(letters[::2])
+
+
+
+
