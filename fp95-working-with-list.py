eb2c44f3b8660abcc9cda391057accc37599591f
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2023-01-02 22:59:16 -0600

    Update fp95-working-with-list.py

---
+++ fp95-working-with-list.py
@@ -0,0 +1,13 @@
+#List are great, but i sometimes need more structure in my life
+#las listas son buenas , pero yo aveces necesito mas estructura in mi vida
+
+person3 = {
+            'name':'daniel',
+            'gender':'Male',
+            'Occupation': 'Research',
+            'Home Planet': 'Betelgeuse Seven'
+}
+print(person3)
+print(person3['Home Planet'])
+print(person3['name'])
+
