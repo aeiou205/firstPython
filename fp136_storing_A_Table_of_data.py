394de91fa7329790072fd87387064feee35b65c2
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2023-01-03 23:02:19 -0600

    Add files via upload

---
+++ fp136_storing_A_Table_of_data.py
@@ -0,0 +1,38 @@
+
+person3 = {}
+person3['daniel'] = {'name':'daniel',
+         'gender': 'male',
+         'ocupation':'researcher',
+         'home planet':'planeta tierra'}
+print(person3['daniel'])
+
+
+
+people = {}
+people['suarez'] = {'name':'suarez',
+         'gender': 'male',
+         'ocupation':'researcher',
+         'home planet':'planeta tierra'}
+print(people['suarez'])
+
+
+
+#A dictionary of dictionaries (a.k.a.a table)
+
+people['Nava'] = {'name':'suarez',
+         'gender': 'male',
+         'ocupation':'researcher',
+         'home planet':'planeta tierra'}
+print(people['suarez'])
+
+people['dann'] = {'name':'suarez',
+         'gender': 'male',
+         'ocupation':'researcher',
+         'home planet':'planeta tierra'}
+
+people['dancito'] = {'name':'suarez',
+         'gender': 'male',
+         'ocupation':'researcher',
+         'home planet':'planeta tierra'}
+
+print(people)
