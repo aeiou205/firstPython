22372f3e8039a37cf14c26c802842085aae6ecdd
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2023-01-04 21:18:23 -0600

    Add files via upload

---
+++ fp161_list_tuplas_dict_etc.py
@@ -0,0 +1,27 @@
+#use the list BIF to define an empty list, then assgn some data
+#use la list para definir un previa lista entonces asi asignaras algunos datos
+
+i = list() 
+print(i)
+
+i = [1,2,3]
+print(i)
+
+D = dict()
+print(D)
+
+D = {'first':1, 'second':2 , 'tree': 3}
+print(D)
+
+s = set()
+print(s)
+
+s = {1,2,3}
+print(s)
+
+t = tuple()
+print(t)
+
+t = (1,2,3,4)
+print(t)
+
