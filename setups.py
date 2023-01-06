f16acea76c42311c2ad6ca6ebebd06aeef9ae6de
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2023-01-06 13:16:10 -0600

    Create setups.py

---
+++ setups.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'vsearch',
+    version = '1.0',
+    description='the Head First Python Search Tools',
+    author = 'HF python 2e',
+    author_email = 'hfpy2e@gmail.com',
+    url='headfirstlabs.com',
+    py_modules=['vsearch'],
+)
+
+#tiene que estar en la misma carpeta con vseaarch .. ejecutar primero 
